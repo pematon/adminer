8b0d5c326e1ed22fa936dbc9ccd43840ba7b3832
Peter Knut <peter@pematon.com>
2024-03-18 00:34:13 +0100

    Modernize default design, SVG icons, dark mode

--- adminer/include/design.inc.php
+++ adminer/include/design.inc.php
@@ -28,7 +28,8 @@ function page_header($title, $error = "", $breadcrumb = [], $title2 = "") {
 <!DOCTYPE html>
 <html lang="<?php echo $LANG; ?>" dir="<?php echo lang('ltr'); ?>">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="robots" content="noindex">
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="initial-scale=1"/>
 <title><?php echo $title_page; ?></title>
 <link rel="stylesheet" type="text/css" href="../adminer/static/default.css?<?php echo filemtime("../adminer/static/default.css"); ?>">
 <?php echo script_src("../adminer/static/functions.js?" . filemtime("../adminer/static/functions.js")); ?>
