0e7169116d014230628f1e45e1a13dc48774df8a
Peter Knut <peter@pematon.com>
2024-12-31 01:16:21 +0100

    Add several modes of main navigation
    - Add navigationMode and preferSelection config options.
    - Use <nav> and <menu> tags for navigation.

--- adminer/include/design.inc.php
+++ adminer/include/design.inc.php
@@ -76,7 +76,7 @@ function page_header($title, $error = "", $breadcrumb = [], $title2 = "") {
 <div id="content">
 <?php
 	if ($breadcrumb !== null) {
-		echo '<p id="breadcrumb">';
+		echo '<nav id="breadcrumb">';
 
 		echo '<a href="' . h(HOME_URL) . '" title="', lang('Home'), '">', icon_solo("home"), '</a> » ';
 
@@ -113,6 +113,8 @@ function page_header($title, $error = "", $breadcrumb = [], $title2 = "") {
 
 			echo "$title\n";
 		}
+
+		echo "</nav>";
 	}
 
 	echo "<h2>$title_all</h2>\n";
