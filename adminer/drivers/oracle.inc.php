706a7419b04de4b72aa986d60ccc4354d3095997
Jakub Vrana <jakub@vrana.cz>
2025-02-19 10:42:35 +0100

    Oracle: Remove NOT REGEXP
    https://sourceforge.net/p/adminer/bugs-and-features/804/

--- adminer/drivers/oracle.inc.php
+++ adminer/drivers/oracle.inc.php
@@ -545,7 +545,7 @@ ORDER BY PROCESS
 			'types' => $types,
 			'structured_types' => $structured_types,
 			'unsigned' => array(),
-			'operators' => array("=", "<", ">", "<=", ">=", "!=", "LIKE", "LIKE %%", "IN", "IS NULL", "NOT LIKE", "NOT REGEXP", "NOT IN", "IS NOT NULL", "SQL"),
+			'operators' => array("=", "<", ">", "<=", ">=", "!=", "LIKE", "LIKE %%", "IN", "IS NULL", "NOT LIKE", "NOT IN", "IS NOT NULL", "SQL"),
 			'operator_like' => "LIKE %%",
 			'functions' => array("length", "lower", "round", "upper"),
 			'grouping' => array("avg", "count", "count distinct", "max", "min", "sum"),
