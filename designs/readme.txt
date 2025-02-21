85f9c9371da5b6287620fe9b1c9e5a73feb4cee5
Thierry Daguin <tdaguin@free.fr>
2025-02-21 15:19:53 +0100

    feat: auto-discover designs in AdminerDesigns plugin

--- designs/readme.txt
+++ designs/readme.txt
@@ -1,1 +1,10 @@
 Copy adminer.css alongside Adminer PHP script to use an alternative design.
+
+If you want to easily test all the designs:
+    - Copy all the designs into the "designs" folder (ie designs/ng9/adminer.css)
+    - Install the "designs" plugin (see the plugin installation documentation).
+A dropdown list will appear in the bottom right for selecting your design.
+Once you have chosen your design, it is best to:
+    - Copy the adminer.css file to the root directory,
+    - Disable the "designs" plugin,
+    - Delete the designs.
--- plugins/designs.php
+++ plugins/designs.php
@@ -9,14 +9,22 @@
 class AdminerDesigns {
 	/** @access protected */
 	var $designs;
-	
+
 	/**
-	* @param array URL in key, name in value
-	*/
-	function __construct($designs) {
+	 * @param array $designs URL in key, name in value. If not provided, designs will be auto-discovered in ./designs folder.
+	 */
+	function __construct($designs = []) {
 		$this->designs = $designs;
+
+		// Discover available designs.
+		if (empty($this->designs)) {
+			$directories = array_map('basename', glob('./designs/*', GLOB_ONLYDIR));
+			foreach ($directories as $directory) {
+				$this->designs['./designs/' . $directory . '/adminer.css'] = ucfirst($directory);
+			}
+		}
 	}
-	
+
 	function headers() {
 		if (isset($_POST["design"]) && verify_token()) {
 			restart_session();
@@ -24,18 +32,23 @@ class AdminerDesigns {
 			redirect($_SERVER["REQUEST_URI"]);
 		}
 	}
-	
+
 	function css() {
 		$return = array();
 		if (array_key_exists($_SESSION["design"], $this->designs)) {
 			$return[] = $_SESSION["design"];
 		}
+
 		return $return;
 	}
-	
+
 	function navigation($missing) {
 		echo "<form action='' method='post' style='position: fixed; bottom: .5em; right: .5em;'>";
-		echo html_select("design", array("" => "(design)") + $this->designs, $_SESSION["design"], "this.form.submit();");
+		if (!$this->designs) {
+			echo "<div class='message' style='margin-right: 0; cursor: help' title='Copy designs from source code in \"designs\" directory (for example designs/ng9/adminer.css) or deactivate AdminerDesigns plugin.'>No designs available!</div>";
+		} else {
+			echo html_select("design", array("" => "(design)") + $this->designs, $_SESSION["design"], "this.form.submit();");
+		}
 		echo '<input type="hidden" name="token" value="' . get_token() . '">';
 		echo "</form>\n";
 	}
