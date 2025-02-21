1159ccb8b2a13f2d86536700a3c140e4fc73941d
Thierry Daguin <tdaguin@free.fr>
2025-02-21 15:19:53 +0100

    feat: auto-discover designs in 'designs' plugin

--- plugins/designs.php
+++ plugins/designs.php
@@ -9,14 +9,22 @@
 class AdminerDesigns {
 	/** @access protected */
 	var $designs;
-	
+
 	/**
-	* @param array URL in key, name in value
-	*/
-	function __construct($designs) {
+	 * @param array URL in key, name in value. If not provided, designs will be auto-discovered.
+	 */
+	function __construct($designs = []) {
 		$this->designs = $designs;
+
+		// Discover available designs
+		if(empty($this->designs)) {
+			$directories = array_map('basename', glob('./designs/*', GLOB_ONLYDIR));
+			foreach ($directories as $directory) {
+				$this->designs['./designs/' . $directory . '/adminer.css'] = ucfirst($directory);
+			}
+		}
 	}
-	
+
 	function headers() {
 		if (isset($_POST["design"]) && verify_token()) {
 			restart_session();
@@ -24,15 +32,21 @@ class AdminerDesigns {
 			redirect($_SERVER["REQUEST_URI"]);
 		}
 	}
-	
+
 	function css() {
+		if(empty($this->designs)) {
+			echo "<br> No designs available !! Please copy designs from source code in 'designs' directory (for example designs/ng9/adminer.css) or desactivate 'designs' plugin";
+			exit;
+		}
+
 		$return = array();
 		if (array_key_exists($_SESSION["design"], $this->designs)) {
 			$return[] = $_SESSION["design"];
 		}
+
 		return $return;
 	}
-	
+
 	function navigation($missing) {
 		echo "<form action='' method='post' style='position: fixed; bottom: .5em; right: .5em;'>";
 		echo html_select("design", array("" => "(design)") + $this->designs, $_SESSION["design"], "this.form.submit();");
